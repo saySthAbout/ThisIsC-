163f0510ac485c4f9615a5e7b566351159ee06c3
jinah_kwak <saysthabout@gmail.com>
2021-01-11 11:53:02 +0900

    for문

---
+++ 05장/For/For/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace For
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int i = 0; i< 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
