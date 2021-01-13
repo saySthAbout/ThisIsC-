bef5c5baf3f154678a2c65319c8b6f8c4ea7ec0b
jinah_kwak <saysthabout@gmail.com>
2021-01-13 23:53:27 +0900

    메소드로 코드 간추리기 : 연습문제 6장

---
+++ 06장/Ex6_2/Ex6_2/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex6_2
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, ref mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(
+            double a, double b, double c,
+            double d, double e, ref double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
---
+++ 06장/Ex6_3/Ex6_3/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex6_3
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int resultA = 0;
+
+            Plus(a, b, out resultA);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, resultA);
+
+            double x = 2.4;
+            double y = 3.1;
+            double resultB = 0;
+
+            Plus(x, y, out resultB);    // 오버로드가 필요한 메소드입니다.
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, resultB);
+        }
+
+        public static void Plus(int a, int b, out int c)
+        {
+            c = a + b;
+        }
+
+        public static void Plus(double a, double b, out double c)
+        {
+            c = a + b;
+        }
+    }
+}
