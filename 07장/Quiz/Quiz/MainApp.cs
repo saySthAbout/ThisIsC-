eee3cacbef8ecdbd10626a432bbe7838de015544
jinah_kwak <saysthabout@gmail.com>
2021-01-14 01:33:08 +0900

    비타민 퀴즈 7-1
    string 클래스에 문자열 매개 변수를 입력받아 기존의 문자열 뒤에 붙여 반환하는 Append() 확장 메소드를 추가해 보세요.

---
+++ 07장/Quiz/Quiz/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Quiz
+{
+    public static class stringExtension
+    {
+        public static string Append(this string org, string appendStr)
+        {
+            return org + appendStr;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            Console.WriteLine(hello.Append(", World!"));     // "Hello, World!" 출력
+        }
+    }
+}
