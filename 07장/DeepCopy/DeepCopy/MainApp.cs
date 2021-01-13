59acf629df7585c28d05c2dd361382c2d39e5576
jinah_kwak <saysthabout@gmail.com>
2021-01-14 00:17:11 +0900

    ICloneable.Clone() 메소드
    .NET 프레임워크의 System 네임스페이스에는 ICloneable이라는 인터페이스가 있습니다.

--- 07장/DeepCopy/DeepCopy/MainApp.cs
+++ 07장/DeepCopy/DeepCopy/MainApp.cs
@@ -50,6 +50,9 @@ namespace DeepCopy
                 Console.WriteLine($"{source.MyField1} {source.MyField2}");
                 Console.WriteLine($"{target.MyField1} {target.MyField2}");
 
+                // ICloneable.Clone() 메소드
+                // "깊은 복사" 기능을 가질 클래스가 .NET 프레임워크의 다른 유틸리티 클래스나 다른 프로그래머가 작성한 코드와
+                // 호환되도록 하고 싶다면 ICloneable을 상속하도록 하는 것이 좋습니다. ICloneable 인터페이스는 Clone() 메소드 하나만 갖고 있습니다.
             }
         }
     }
