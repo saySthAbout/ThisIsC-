87b019cbf42b0545e41caa1d8af28a218f6f3242
jinah_kwak <saysthabout@gmail.com>
2021-01-14 18:47:34 +0900

    연습문제 7장

---
+++ 07장/Ex7_2/Ex7_2/C.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex7_2
+{
+    class A
+    {
+
+    }
+    class B : A
+    {
+
+    }
+
+    class C
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            A c = new B();
+            B d = (B)new A(); // 여기서 에러난다. // B d = (B)new A(); --> 이렇게 casting 연산자를 이용해 해결할 수 있다.
+        }
+    }
+}
