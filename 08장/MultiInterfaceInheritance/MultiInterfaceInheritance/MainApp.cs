e194453a403ae4d1358bf171e2d4fe8246d32adc
jinah_kwak <saysthabout@gmail.com>
2021-01-14 19:00:00 +0900

    여러 개의 인터페이스, 한꺼번에 상속하기
    클래스는 여러 클래스를 한꺼번에 상속할 수 없습니다. 이른바 "죽음의 다이아몬드"라는 문제 때문입니다.
    
    다중 상속은 이런 재앙을 불러올 수 있는 위험을 갖습니다. 이런 재앙은 소스 코드 수준에서 방지할 수 있다면 가장 좋습니다. 그래서 C#은 클래스의 다중 상속을 허용하지 않고 있습니다.
    
    인터페이스는 내용이 아닌 외형을 물려줍니다 속은 어떨지 몰라도 겉모습만큼은 정확하게 자신을 닮기를 강제합니다. 따라서 죽음의 다이아몬드 같은 문제도 발생하지 않습니다.
    
    그래도 저는 여러 클래스로부터 구현을 물려받고 싶은데요? >> 포함(Containment) 이라는 기법을 쓰면 됩니다.
    클래스 안에 물려받고 싶은 기능을 가진 클래스들을 필드로 선언해 넣는 것이지요.

---
+++ 08장/MultiInterfaceInheritance/MultiInterfaceInheritance/MainApp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MultiInterfaceInheritance
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class Plane : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+
+    class Car : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+    }
+
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.Fly();
+
+            IRunnable runnable = car as IRunnable;
+            runnable.Run();
+
+            IFlyable flyable = car as IFlyable;
+            flyable.Fly();
+        }
+    }
+}
