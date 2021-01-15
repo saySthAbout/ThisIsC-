5699ec88be373c66fa526504398e7ed893d1df5a
jinah_kwak <saysthabout@gmail.com>
2021-01-15 13:46:42 +0900

    파일 경로 잘못된 것 바로잡음

---
+++ 09장/Ex9_1/Ex9_1/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex9_1
+{
+    class NameCard
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            NameCard MyCard = new NameCard();
+
+            MyCard.Age = 24;
+            MyCard.Name = "상현";
+
+            Console.WriteLine("나이 : {0}", MyCard.Age);
+            Console.WriteLine("이름 : {0}", MyCard.Name);
+        }
+    }
+}
---
+++ 09장/Ex9_2/Ex9_2/MainApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex9_2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var nameCard = new { Name = "박상현", Age = 17 };
+            Console.WriteLine("이름:{0}, 나이:{1}", nameCard.Name, nameCard.Age);
+
+            var complex = new { Real = 3, Imaginary = -12 };
+            Console.WriteLine("Real:{0}, Imaginary:{1}", complex.Real, complex.Imaginary);
+
+        }
+    }
+}
