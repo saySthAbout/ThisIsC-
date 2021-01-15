49775206ff052541f795aac4c678a35fbdc74fca
jinah_kwak <saysthabout@gmail.com>
2021-01-16 03:24:46 +0900

    10장 연습문제

---
+++ 10장/Ex10_2/Ex10_2/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex10_2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[2, 2] { { 3, 2 }, { 1, 4 } };
+            int[,] b = new int[2, 2] { { 9, 2 }, { 1, 7 } };
+            int[][] times = new int[2][];
+
+            times[0] = new int[2] { (a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0]), (a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1]) };
+            times[1] = new int[2] { (a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0]), (a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1]) };
+
+            foreach(int[] arr in times)
+            {
+                foreach(int i in arr)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            } 
+            // 29 20
+            // 13 30
+        }
+    }
+}
---
+++ 10장/Ex10_5/Ex10_5/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Ex10_5
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht["회사"] = "Microsoft";
+            ht["URL"] = "https://www.microsoft.com/ko-kr/";
+
+            Console.WriteLine(" 회사 : {0}", ht["회사"]);   // 회사 : Microsoft
+            Console.WriteLine(" URL : {0}", ht["URL"]);     // URL : https://www.microsoft.com/ko-kr/
+        }
+    }
+}
