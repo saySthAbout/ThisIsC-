8c1a1623dd601fd4d8a3997ec2073ec53a8a644e
jinah_kwak <saysthabout@gmail.com>
2021-01-16 02:08:55 +0900

    비타민 퀴즈 10-2
    2차원 배열을 foreach 문에 넣고 각 요소의 데이터를 출력해 보세요.  어떤 결과가 나옵니까? > 그냥 1차원 2차원 가리지 않고 접근합니다.

---
+++ 10장/Quiz10-2/Quiz10-2/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz10_2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            foreach(int i in arr)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
