83898b74c7acedc5a349ea0509d1bc912075af43
JinAh <saysthabout@gmail.com>
2021-05-23 22:26:38 +0900

    13.7 대리자와 이벤트
    앞에서 살펴봤듯이, 이벤트는 대리자에 event 키워드로 수식해서 선언한 것에 불과합니다. 언뜻 봤을 때는 둘의 차이가 별로 없는 것 같은데, 마이크로소프트의 C# 팀은 왜 이벤트를 언어에 추가했을까요?
    이벤트가 대리자와 가장 크게 다른 점은 바로 이벤트는 외부에서 직접 사용할 수 없다는 데 있습니다.
    이벤트는 public 한정자로 선언되어 있어도 자신이 선언되어 있는 클래스 외부에서는 호출이 불가능합니다.
    반면에 대리자는 public 이나 internal로 수식되어 있으면 클래스 외부에서라도 얼마든지 호출이 가능하지요.
    
    대리자와는 달리 이벤트가 호출될 수 없다는 사실은 견고한 이벤트 기반 프로그래밍에 대한 기대를 가능하게 합니다. 예를 들어 생각해 보세요. 여러분이 네트워크 상태 변화에 대한 사건을 알리는 클래스를 작성해서 동료에게 줬다고 해봅시다. 이벤트를 객체 외부에서 임의로 호출할 수 있게 된다면 동료 프로그래머가 여러분이 작성한 클래스의 객체가 감시하고 있는 실제 네트워크 상태와는 상관없이 객체의 외부에서 허위로 네트워크 상태 변화 이벤트를 일으킬 수 있게 됩니다. 이것은 실제로 객체의 상태를 바꾸는 것보다 더 나쁩니다. 객체의 상태를 허위로 나타낼 수 있으니 말입니다.
    이런 위협은 대리자로는 막을 수가 없습니다. 이벤트라야 가능하지요. 따라서 대리자는 대리자대로 콜백 용도로 사용하고, 이벤트는 이벤트대로 객체의 상태 변화나 사건의 발생을 알리는 용도로 구분 해서 사용해야 합니다.

---
+++ 13장_대리자와 이벤트/Ex13_1/Ex13_1/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex13_1
+{
+    delegate int MyDelegate(int a, int b);
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate Callback;
+
+            Callback = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            Console.WriteLine(Callback(3, 4));
+
+            Callback = delegate (int c, int d)
+            {
+                return c - d;
+            };
+
+            Console.WriteLine(Callback(7, 5));
+        }
+    }
+}
---
+++ 13장_대리자와 이벤트/Ex13_2/Ex13_2/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex13_2
+{
+    delegate void MyDelegate(int a);
+
+    class Market
+    {
+        public event MyDelegate CustomerEvent;
+        
+        public void BuySomething(int CustomerNo)
+        {
+            if (CustomerNo == 30)
+                CustomerEvent(CustomerNo);
+        }
+    }
+
+    class MainApp
+    {
+        static public void MyHandler(int no)
+        {
+            Console.WriteLine($"축하합니다! {no}번째 고객 이벤트에 당첨되셨습니다.");
+        }
+
+        static void Main(string[] args)
+        {
+            Market market = new Market();
+            market.CustomerEvent += new MyDelegate(MyHandler);
+
+            for (int customerNo = 0; customerNo < 100; customerNo += 10)
+                market.BuySomething(customerNo);
+        }
+    }
+}
