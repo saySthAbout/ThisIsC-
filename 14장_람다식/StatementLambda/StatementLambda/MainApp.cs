81b939c03dcce5109046815938ac9a6f8c22b45b
JinAh <saysthabout@gmail.com>
2021-06-02 12:43:30 +0900

    14.3 문 형식의 람다식
    문 형식의 람다 식(Statement Lambda)은 => 연산자의 오른편에 식 대신 {와 }로 둘러싸인 코드 블록이 위치합니다. 형식은 다음과 같습니다.
    
    (매개_변수_목록) => {
    문장1;
    문장2;
    문장3;
    ... 
    }
    
    다음은 문 형식의 람다식 예제 코드입니다. 이 예제에서는 반환 형식과 매개 변수가 없는 대리자를 사용하고 있습니다. 식 형식의 람다식으로는 반환 형식이 없는 무명 함수를 만들 수 없지만, 문 형식의 람다식을 이용하면 가능합니다.

---
+++ 14장_람다식/StatementLambda/StatementLambda/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StatementLambda
+{
+    class MainApp
+    {
+        delegate string Concatenate(string[] args);
+
+        static void Main(string[] args)
+        {
+            Concatenate concat =
+                (arr) =>
+                {
+                    string result = "";
+                    foreach (string s in arr)
+                        result += s;
+
+                    return result;
+                };
+
+            Console.WriteLine( concat(new string[] { "아버지가", "방에", "들어가신다" }) );
+        }
+    }
+}
